d8deeb1f89d8a07d4576c3b74ab53bebdf108be6
Bisrat Dereje <bdereje996@gmail.com>
2025-07-09 00:08:34 +0300

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const authRoutes = require('./routes/auth');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ Connected to MongoDB Atlas"))
+  .catch((err) => console.error("❌ MongoDB error:", err));
+
+app.use('/api', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
